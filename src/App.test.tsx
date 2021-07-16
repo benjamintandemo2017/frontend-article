36b0aac042582dd7f92b4a3063139d8892f7ee36
benjamintandemo2017 <benjamintandemo2017@gmail.com>
2021-07-17 00:57:02 +1000

    Fix unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import * as Constants from './common/Constants';
 
-test('renders learn react link', () => {
+test('renders respective contents', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const TitleElement = screen.getByText(Constants.mainTitle);
+  const Card1Element = screen.getByText(Constants.cardTitle1);
+  const Card2Element = screen.getByText(Constants.cardTitle2);
+  const Card3Element = screen.getByText(Constants.cardTitle3);
+
+  expect(TitleElement).toBeInTheDocument();
+  expect(Card1Element).toBeInTheDocument();
+  expect(Card2Element).toBeInTheDocument();
+  expect(Card3Element).toBeInTheDocument();
 });
