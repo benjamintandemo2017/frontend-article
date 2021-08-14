3cf074ecd06a13121c25328ccd9c56ae14981477
benjamintandemo2017 <benjamintandemo2017@gmail.com>
2021-08-15 00:06:34 +1000

    Update unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import * as Constants from './common/Constants';
+import {articles} from './common/articles'
 
 test('renders respective contents', () => {
   render(<App />);
   const TitleElement = screen.getByText(Constants.mainTitle);
-  const Card1Element = screen.getByText(Constants.cardTitle1);
-  const Card2Element = screen.getByText(Constants.cardTitle2);
-  const Card3Element = screen.getByText(Constants.cardTitle3);
+  const Card1Element = screen.getByText(articles[0].title);
+  const Card2Element = screen.getByText(articles[1].title);
+  const Card3Element = screen.getByText(articles[2].title);
 
   expect(TitleElement).toBeInTheDocument();
   expect(Card1Element).toBeInTheDocument();
